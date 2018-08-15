ec50489a7c6ff7440c64c73c78e7dda3564a345c
Wesley Gomes <wesleycgs@gmail.com>
2018-08-15 15:45:51 -0300

    Init Xcode project

---
+++ GitHub Stars/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHub Stars
+//
+//  Created by Wesley Gomes on 15/08/18.
+//  Copyright © 2018 Wesley Gomes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
